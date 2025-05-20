e0f456cb32f8c04d04736c215f6757f37e934ef4
Yashith <yashith.sasmithasysenact@gmail.com>
2025-05-20 14:53:15 +0530

    test push

--- src/main/java/com/temenos/smib/reports/recovery/test.java
+++ src/main/java/com/temenos/smib/reports/recovery/test.java
@@ -1,8 +1,11 @@
 package com.temenos.smib.reports.recovery;
 
+import com.temenos.t24.api.tables.ebsmibgrouploandetails.EbSmibGroupLoanDetailsRecord;
+
 public class test {
 private void psv() {
 	System.out.println("adsffad");
+	EbSmibGroupLoanDetailsRecord gropuLoanDet = new EbSmibGroupLoanDetailsRecord();
 
 }
 }
