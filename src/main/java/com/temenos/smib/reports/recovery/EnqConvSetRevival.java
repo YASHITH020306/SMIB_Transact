037c52ffa34108d6b1eae1a23f50d21f00d3dab1
Yashith <yashith.sasmithasysenact@gmail.com>
2025-05-20 14:37:17 +0530

    Sample Code

---
+++ src/main/java/com/temenos/smib/reports/recovery/EnqConvSetRevival.java
@@ -0,0 +1,25 @@
+//package com.temenos.smib.reports.recovery;
+//
+//import java.util.List;
+//
+//import com.temenos.api.TStructure;
+//import com.temenos.t24.api.complex.eb.enquiryhook.EnquiryContext;
+//import com.temenos.t24.api.complex.eb.enquiryhook.FilterCriteria;
+//import com.temenos.t24.api.hook.system.Enquiry;
+//
+///**
+// * TODO: Document me!
+// *
+// * @author Dell
+// *
+// */
+//public class EnqConvSetRevival extends Enquiry {
+//
+//    @Override
+//    public String setValue(String value, String currentId, TStructure currentRecord,
+//            List<FilterCriteria> filterCriteria, EnquiryContext enquiryContext) {
+//        // TODO Auto-generated method stub
+//        return super.setValue(value, currentId, currentRecord, filterCriteria, enquiryContext);
+//    }
+//
+//}
